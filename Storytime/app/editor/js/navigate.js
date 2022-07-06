bdc316d995961490ef9ee2d8ad37b2eeaf7b27e7
Redwars22 <andrewpereira.megabyte@gmail.com>
2022-07-06 19:44:40 -0300

    chore(0.1.9-DEV): adicionado o path e os arquivos para a funcionalidade do Brainstorm

--- Storytime/app/editor/js/navigate.js
+++ Storytime/app/editor/js/navigate.js
@@ -1,4 +1,16 @@
 function handleNavigate(path){
     if(path == 'settings')
         window.location.replace('../settings/settings.html');
+
+    if(path == 'brainstorm')
+        window.location.replace('../brainstorm/brainstorm.html');
+
+    // if(path == '')
+    //     window.location.replace('');
+
+    // if(path == '')
+    //     window.location.replace('');
+
+    // if(path == '')
+    //     window.location.replace('');
 }
