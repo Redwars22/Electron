d6a31cf5a954cccb3880ad270aa9ecb6842529a7
Redwars22 <andrewpereira.megabyte@gmail.com>
2022-07-14 20:00:16 -0300

    feat(0.3.0-DEV): adicionado componentes de cena, narração e comentário

--- Storybook/app/editor/js/getSettings.js
+++ Storybook/app/editor/js/getSettings.js
@@ -5,9 +5,14 @@ window.onload = ()=>{
         document.querySelector('body').style['background-color'] = '#c3cad6';
         document.querySelector('nav').style['color'] = 'black';
         document.querySelector('.button-group').style['color'] = 'black';
-    } else {
+    } else if(theme == "dark") {
         document.querySelector('body').style['background-color'] = '#001d25';
         document.querySelector('body').style['color'] = 'white';
         document.querySelector('.button-group').style['color'] = '#fff';
-    }
+    } else if(theme == "red"){
+        document.querySelector('body').style['background-color'] = '#eb4034';
+        document.querySelector('body').style['color'] = '#ff867d';
+        document.querySelector('.button-group').style['color'] = '#fff';
+
+    } else {}
 };
--- Storybook/app/settings/settings.js
+++ Storybook/app/settings/settings.js
@@ -9,3 +9,18 @@ function handleChangeSettings(){
         window.location.replace('../editor/editor.html')
     }, 1000);
 }
+
+function handleDeleteEverything(){
+    swal.fire({
+        title: 'Espera aí!',
+        text: 'Tem certeza de que deseja deletar todo o conteúdo? OBS: É irreversível!',
+        icon: 'warning',
+        confirmButtonText: 'Sim, deletar!',
+        showCancelButton: true,
+        cancelButtonText: 'Não, cancelar!',
+    }).then((result)=> {
+        if(result.isConfirmed)
+        swal.fire({title: 'deletou', icon: 'warning'});
+        else swal.fire({title: 'não deletou', icon: 'warning'});
+    });
+}
