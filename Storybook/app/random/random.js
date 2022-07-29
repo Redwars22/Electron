10dbada40dfeaa99916bd75eda4021beacfa8f78
André Pereira <andrewpereira.megabyte@gmail.com>
2022-07-28 22:17:37 -0300

    feat(1.3.0): adicionando módulo para gerar personagens e adicioná-los à lista

--- Storybook/app/random/random.js
+++ Storybook/app/random/random.js
@@ -2,21 +2,70 @@
 
 class Character {
     name;
-    age;
-    personality;
-    weight;
-    birthContinent;
-    height;
-    bloodType;
-    birthDate;
+    personalityArray = [
+    'ESTJ',
+    'ENTJ',
+    'ESFJ',
+    'ENFJ',
+    'ISTJ',
+    'INFJ',
+    'INTJ',
+    'INFJ',
+    'ESTP',
+    'ESFP',
+    'ENTP',
+    'ENFP',
+    'ISTP',
+    'ISFP',
+    'INTP',
+    'INFP',
+    ];
+    bloodTypesArray = ['A+', 'A-', 'B+', 'B-', 'AB', 'O+', 'O-'];
+    monthsArray = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+    ]
 
-    personalityArray;
-    bloodTypesArray;
+    getAge(){
+        const randomAge = Math.floor(Math.random() * (100 + 1));
+        return (randomAge == 0 ? 1 : randomAge);
+    }
 
-    getCharacters(){}
+    getRandomValue(min, max){
+        const value = Math.random() * (max + 1);
+        return (value < min ? value + 1.0 : value).toFixed(2);
+    }
+
+    getBloodType(){
+        return this.bloodTypesArray[Math.floor(Math.random() * this.bloodTypesArray.length)];
+    }
+
+    getBirthMonth(){
+        return this.monthsArray[Math.floor(Math.random() * this.monthsArray.length)];
+    }
+
+    getBirthPlace(){
+        return null;
+    }
+
+    getPersonality(){
+        return this.personalityArray[Math.floor(Math.random() * this.personalityArray.length)];
+    }
 
     getCharacterInfo(){
-        return 'aaaa';
+        /*return {
+            age: this.getAge(),
+            bloodType: this.getBloodType(),
+            personality: this.getPersonality(),
+            height: this.getRandomValue(1.25, 2.10),
+            weight: this.getRandomValue(48.0, 123.0),
+            birthMonth: this.getBirthMonth(),
+            birthPlace: this.getBirthPlace()
+        }*/
+
+        const personalInfo = `IDADE: ${this.getAge()} \nTIPO SANGUÍNEO: ${this.getBloodType()} \nPERSONALIDADE: ${this.getPersonality()}`
+        const size = `\nALTURA: ${this.getRandomValue(1.25, 2.10)} \nPESO: ${this.getRandomValue(48.0, 123.0)}`;
+        const birthInfo = `\nMÊS DE NASCIMENTO: ${this.getBirthMonth()}\nCONTINENTE EM QUE NASCEU: `
+        return personalInfo + size + birthInfo;
     }
 
     generateCharacter(){}
@@ -36,16 +85,22 @@ class Character {
 
         if(name){
             this.name = name;
-
+            const character = this.getCharacterInfo();
             Swal.fire({
                 title: this.name,
-                text: this.getCharacterInfo(),
+                text: character,
                 confirmButtonText: 'Adicionar à lista',
                 showCancelButton: true,
                 cancelButtonText: 'Cancelar'
-            })
+            }).then((result)=> {
+                if(result.isConfirmed){
+                    document.querySelector('.characters-list').innerHTML += `<div class="character-container" contenteditable><h1 class="character-name">${this.name}</h1>${character}</div>`;
+                }
+            });
         }
     }
+
+    getCharacters(){}
 
     deleteAllCharacters(){}
 }
